4db1d10dd5a65d873f163245391bc27742242eb3
Andrew <adllivings@gmail.com>
2021-07-08 15:19:09 -0600

    First Upload

---
+++ js/select.js
@@ -0,0 +1,42 @@
+document.write(
+  `
+  <div class="select-menu" id="modal-program-select">
+
+<div class="wrapper">
+ <input type="radio" name="select" id="option-1" checked>
+ <input type="radio" name="select" id="option-2">
+ <input type="radio" name="select" id="option-3">
+ <input type="radio" name="select" id="option-4">
+   <label for="option-1" class="option option-1">
+     <div class="dot"></div>
+      <span><img src="https://raw.githubusercontent.com/gregrylivingston/MindFoundry/main/img/avatar/avatar_mcawil.png"></span>
+      <span>Minecraft Survival World<br>
+            Grades 6-8<br>
+            Starts July 13th @ 10AM Central</span>
+      </label>
+   <label for="option-2" class="option option-2">
+     <div class="dot"></div>
+      <span><img src="https://gregrylivingston.github.io/MindFoundry/img/avatar/avatar_rocketleague.png"></span>
+      <span>Rocket League Boot Camp<br>
+            Grades 3-5<br>
+            Starts July 13th @ 10AM Central</span>
+   </label>
+   <label for="option-3" class="option option-3">
+     <div class="dot"></div>
+      <span><img src="https://raw.githubusercontent.com/gregrylivingston/MindFoundry/main/img/avatar/avatar_mcawil.png"></span>
+      <span>Minecraft Ninja Challenge<br>
+            Grades 6-8<br>
+            Starts July 19th @ 2PM Central</span>
+   </label>
+   <label for="option-4" class="option option-4">
+     <div class="dot"></div>
+      <span><img src="https://gregrylivingston.github.io/MindFoundry/img/avatar/avatar_rocketleague.png"></span>
+      <span>Rocket League Flight Camp<br>
+            Grades 6-8<br>
+            Starts July 19th @ 10AM Central</span>
+   </label>
+</div>
+
+</div>
+`
+);
