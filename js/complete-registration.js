0df8473dae440f4f864b276614a3b55852e60788
Andrew <adllivings@gmail.com>
2021-07-08 20:05:29 -0600

    Mind Foundry Logo, Complete-registration changes

--- js/complete-registration.js
+++ js/complete-registration.js
@@ -7,8 +7,8 @@ document.write(
   <input class="form-control" type="text" name="username" id="username" placeholder="Username" required=""><br>
   <input class="form-control" type="text" name="password" id="password" placeholder="Password" required=""><br>
   <input class="form-control" type="text" name="re-enter-password" id="re-enter-password" placeholder="Re-enter Password" required=""><br>
-  <div style="width:100%">
-  <button data-dismiss="modal" type="submit" onclick="registration()" class="btn header-btn-sign ml-2" tabindex="0" style="font-family:'Rubik';font-weight:normal;font-size:24px;width:400px;">Join</button>
+  <div style="align:centered;">
+  <button data-dismiss="modal" type="submit" onclick="registration()" class="btn header-btn-sign ml-2" tabindex="0" style="font-family:'Rubik';font-weight:normal;font-size:24px;">Join</button>
   </div>
 </div>
 `
